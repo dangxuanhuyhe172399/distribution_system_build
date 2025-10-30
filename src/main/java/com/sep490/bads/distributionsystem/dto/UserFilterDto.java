d9f401de979240ed1899f246c9fcbe8eb8daa204
truongthinh <lecongtruongthinh@gmail.com>
2025-10-30 11:02:26 +0700

    feat(User controller): upda export

--- src/main/java/com/sep490/bads/distributionsystem/dto/UserFilterDto.java
+++ src/main/java/com/sep490/bads/distributionsystem/dto/UserFilterDto.java
@@ -2,13 +2,15 @@ package com.sep490.bads.distributionsystem.dto;
 
 import com.sep490.bads.distributionsystem.entity.type.UserStatus;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Data
 public class UserFilterDto {
-    private String fullName;
+    private String q;
     private UserStatus status;
-    private LocalDate from;        // filter createdAt >= from
-    private LocalDate to;          // filter createdAt <  to+1
+    private Long roleId;                            // lọc role
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) private LocalDate from; // >= from 00:00
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) private LocalDate to;   // <  to+1 00:00
 }
