c4be54d1633a48038b710640f301d6041cca059f
truongthinh <lecongtruongthinh@gmail.com>
2025-10-30 11:02:26 +0700

    feat(User controller): upda export

--- src/main/java/com/sep490/bads/distributionsystem/controller/UserController.java
+++ src/main/java/com/sep490/bads/distributionsystem/controller/UserController.java
@@ -4,29 +4,26 @@ import com.sep490.bads.distributionsystem.dto.UserCreateDto;
 import com.sep490.bads.distributionsystem.dto.UserDto;
 import com.sep490.bads.distributionsystem.dto.UserProfileUpdateDto;
 import com.sep490.bads.distributionsystem.dto.UserUpdateDto;
-import com.sep490.bads.distributionsystem.entity.User;
-import com.sep490.bads.distributionsystem.entity.type.UserGender;
+import com.sep490.bads.distributionsystem.dto.UserFilterDto;
 import com.sep490.bads.distributionsystem.entity.type.UserStatus;
 import com.sep490.bads.distributionsystem.response.ResultResponse;
-import com.sep490.bads.distributionsystem.security.service.UserDetailsImpl;
 import com.sep490.bads.distributionsystem.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.hibernate.usertype.UserType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.time.LocalDate;
 
 @RestController
 @RequestMapping("v1/public/user")
@@ -35,8 +32,16 @@ public class UserController extends BaseController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Lấy danh sách User và lọc danh sách")
+    @GetMapping
+    public ResultResponse<Page<UserDto>> getAllUserAndFilter(
+            @ModelAttribute UserFilterDto filter,
+            @PageableDefault(sort="createdAt", direction=Sort.Direction.DESC) Pageable pageable) {
+        return ResultResponse.success(userService.search(pageable, filter));
+    }
+
     @Operation(summary = "Lấy danh sách users")
-    @GetMapping("")
+    @GetMapping("/getAllUser")
     public ResultResponse<Page<UserDto>> getAllUsers(
             @PageableDefault(sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
         return ResultResponse.success(userService.getAllUsers(pageable));
@@ -98,5 +103,25 @@ public class UserController extends BaseController {
         var uploadAvatar = getUserDetails(authentication);
         userService.updateUserAvatar(uploadAvatar, file);
         return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/exportData")
+    public ResponseEntity<Resource> exportData(@ModelAttribute UserFilterDto filter) {
+        ByteArrayResource file = userService.exportFile(filter);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=users.csv")
+                .contentType(MediaType.parseMediaType("text/csv"))
+                .body(file);
+    }
+    @PatchMapping("/{id}/activate")
+    public ResponseEntity<Void> activate(@PathVariable Long id){
+        userService.updateUserStatus(id, UserStatus.ACTIVE);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/deactivate")
+    public ResponseEntity<Void> deactivate(@PathVariable Long id){
+        userService.updateUserStatus(id, UserStatus.INACTIVE);
+        return ResponseEntity.noContent().build();
     }
 }
