89fff84cbdef63465b7e6974ba06216b9548e0e9
truongthinh <lecongtruongthinh@gmail.com>
2025-11-04 09:26:50 +0700

    feat(zalo OA): add customer

--- src/main/java/com/sep490/bads/distributionsystem/repository/CustomerRepository.java
+++ src/main/java/com/sep490/bads/distributionsystem/repository/CustomerRepository.java
@@ -1,13 +1,17 @@
 package com.sep490.bads.distributionsystem.repository;
 
 import com.sep490.bads.distributionsystem.entity.Customer;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.data.domain.*;
+import com.sep490.bads.distributionsystem.entity.CustomerType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
+    Optional<Customer> findByPhone(String phone);
+    Optional<CustomerType> findByName(String name);
     @Query("SELECT c FROM Customer c WHERE c.status = 'ACTIVE'")
     Page<Customer> findAllActive(Pageable pageable);
 
