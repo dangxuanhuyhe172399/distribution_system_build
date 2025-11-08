f911f392592e18f3c966f5a6fb60a6be1f4f407d
huydxhe172399 <huydxhe172399@fpt.edu.vn>
2025-11-08 22:24:33 +0700

    fix config

---
+++ src/main/java/com/sep490/bads/distributionsystem/config/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.sep490.bads.distributionsystem.config.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Áp dụng cho tất cả các đường dẫn (endpoint)
+                .allowedOrigins("http://localhost:5173") // Cho phép origin này được gọi
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Cho phép các phương thức này
+                .allowedHeaders("*") // Cho phép tất cả các header
+                .allowCredentials(true); // Cho phép gửi cookie (nếu bạn dùng session)
+    }
+}
